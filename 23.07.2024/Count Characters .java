37ac069cf7e7b5d1625b69a7b8af489f2a47f3bb
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-23 15:03:22 +0530

    Create  Count Characters .java

---
+++ 23.07.2024/Count Characters .java
@@ -0,0 +1,7 @@
+public class CharacterCount {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int count = str.length();
+        System.out.println("Number of characters: " + count);
+    }
+}
