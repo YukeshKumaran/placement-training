bd42862f033df5dd42df5b237bb84c5d35348ceb
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-23 14:59:42 +0530

    Create Find LCM.java

---
+++ 23.07.2024/Find LCM.java
@@ -0,0 +1,14 @@
+public class LCM {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 15;
+        int lcm = (a > b) ? a : b;
+        while (true) {
+            if (lcm % a == 0 && lcm % b == 0) {
+                System.out.println("LCM: " + lcm);
+                break;
+            }
+            lcm++;
+        }
+    }
+}
