b2ac1b294265e1c88e334d4270988bb170759343
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-24 21:54:31 +0530

    Create Last Occurrence of an Element.c

---
+++ 24.07.2024/Last Occurrence of an Element.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, element, index = -1;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to find: ");
+    scanf("%d", &element);
+    for (i = n - 1; i >= 0; i--) {
+        if (arr[i] == element) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1)
+        printf("Last occurrence of %d is at index %d\n", element, index);
+    else
+        printf("%d not found in the array\n", element);
+    return 0;
+}
